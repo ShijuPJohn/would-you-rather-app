afc0fc848fc5d8edb3441a5a721ff9f2f1998d8b
ShijuPJohn <shijupjohn4u@gmail.com>
2021-02-11 19:37:26 +0530

    none

--- src/components/HAppBar.js
+++ src/components/HAppBar.js
@@ -5,8 +5,6 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import Box from '@material-ui/core/Box';
-import Container from '@material-ui/core/Container';
 import Slide from '@material-ui/core/Slide';
 
 function HideOnScroll(props) {
@@ -44,18 +42,6 @@ export default function HAppBar(props) {
                 </AppBar>
             </HideOnScroll>
             <Toolbar />
-{/*            <Container>*/}
-{/*                <Box my={2}>*/}
-{/*                    {[...new Array(50)]*/}
-{/*                        .map(*/}
-{/*                            () => `Cras mattis consectetur purus sit amet fermentum.*/}
-{/*Cras justo odio, dapibus ac facilisis in, egestas eget quam.*/}
-{/*Morbi leo risus, porta ac consectetur ac, vestibulum at eros.*/}
-{/*Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,*/}
-{/*                        )*/}
-{/*                        .join('\n')}*/}
-{/*                </Box>*/}
-{/*            </Container>*/}
         </React.Fragment>
     );
 }
